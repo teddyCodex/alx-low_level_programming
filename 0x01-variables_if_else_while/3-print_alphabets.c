efc85cf1f3a212634c2fb65e64f14c554d959b30
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-14 17:40:40 -0700

    program that prints the alphabet in lowercase, and then in uppercase, followed by a new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i = 'a';
+	char j = 'A';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	while (j <= 'Z')
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
