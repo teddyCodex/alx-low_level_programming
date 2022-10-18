423dafe82080602813134fd78fdd52cad09a612c
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-18 14:44:24 -0700

    fixed comments in file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,9 @@
 
 /**
  * _islower - Entry point
- * int c - value to check
- * Return: Always 0 (Success)
+ * Function to check if a value is lowercase or not
+ * @c: value to be checked
+ * Return: 1 if true and 0 if false.
  */
 int _islower(int c)
 {
