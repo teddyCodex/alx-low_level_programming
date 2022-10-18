da3784f1353508a8c1021321d45208983e60f935
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-18 12:27:34 -0700

    first attempt

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,3 +5,16 @@
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
