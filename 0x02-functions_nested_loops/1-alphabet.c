28daced42ea069f19023d7ee3d40fd0b1b3bd0b3
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-18 12:59:41 -0700

    task 1
    program that prints alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
