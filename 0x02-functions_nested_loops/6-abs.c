46d010d6dcdff3eb5f689093a6fcbba96e2731f4
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-18 16:19:17 -0700

    task 6 - 1st attempt

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: value
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	{
+		return (n * -1);
+	}
+}
