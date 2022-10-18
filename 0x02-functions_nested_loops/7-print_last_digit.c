c740bec8f2161e63749181b1a596871f07b12b4a
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-18 16:31:37 -0700

    task 7 - 1st attempt

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * program to print the last digit of an integer
+ * @n: value to be evaluated
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
