19b9f45116e2e297bba5e7dd49c2529eac1bb5c6
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-20 08:10:24 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,12 @@
 /**
  * print_numbers - Entry point
  * function that prints numbers from 0 to 9 using _putchar
- * @i: value to be evaluated
  * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
-	int i;
+	char i;
 
 	for (i = 0; i <= '9'; i++)
 		_putchar(i);
