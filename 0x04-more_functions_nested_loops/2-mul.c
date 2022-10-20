f4f5555b3cca519745681981d2c4050591dc5b68
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-20 07:53:04 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * function to multiply two integers
+ * @a: integer to be evaluated
+ * @b: second integer to be evaluated
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
