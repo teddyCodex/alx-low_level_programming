bde95a40769a8fcc665b2db1980d55001ea81330
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-19 18:01:58 -0700

    task 9 - 1st attempt

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ * print 9 times table
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int rows, cols, prod, tens, ones;
+
+	row = 0;
+	while (rows <= 9)
+	{
+		cols = 0;
+		while (cols <= 9)
+		{
+			prod = rows * cols;
+			tens = prod / 10;
+			ones = prod % 10;
+			if (cols == 0)
+			{
+				_putchar('0');
+			}
+			else if (prod < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+			cols++;
+		}
+		_putchar('\n');
+		rows++;
+	}
