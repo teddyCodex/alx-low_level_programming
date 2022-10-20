e7a1e81c91ad81e2093d944d730e14ffbdf3f20d
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-19 18:39:46 -0700

    task 11 - 1st attempt

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ * function that prints all natural numbers from n to 98
+ * followed by a comma and space
+ * @n: integer to start count
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		printf("%d", n);
+		if (n == 98)
+		{
+			break;
+		}
+		_putchar(',');
+		_putchar(' ');
+	}
+}
