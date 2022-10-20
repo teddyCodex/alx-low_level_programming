8a59c19a8c9a328b823b545da6942d8600af1fa5
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-20 13:03:10 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,31 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 15 == 0)
+		{
 			printf("FizzBuzz ");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			if (i == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
+	printf("\n");
+
+	return (0);
 }
