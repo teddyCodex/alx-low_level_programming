4fec2840a86abb6f52a66b3bd4e5a0d530f2e5fd
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-20 07:12:00 -0700

    function to check for uppercase char

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * program to check for UPPERCASE character
+ * Return: 1 (true) || 0 (false)
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A' || c == 'Z')
+	{
+		return (1);
+	}
+	{
+		return (0);
+	}
+}
