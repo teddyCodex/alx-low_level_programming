a5d18c92f4f45f4cd131b5fa496f8bbbf49c20ec
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-19 18:15:16 -0700

    task 10 - 1st attempt

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @x: integers to be evaluated
+ * @y: integer to be evaluated
+ * function that adds two integers and returns the result
+ * Return: Always 0 (Success)
+ */
+int add(int x, int y)
+{
+	int res;
+
+	res = x + y;
+	return (res);
+}
