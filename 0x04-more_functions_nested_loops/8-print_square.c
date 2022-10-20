bb301ffded5b8b31b5f955ca7f00a278dfd6e1cd
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-20 12:44:25 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,19 +10,13 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (n <= 0)
-	{
+	if (size <= 0)
 		_putchar('\n');
-	}
-	else
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
 		for (j = 0; j < size; j++)
-		{
 			_putchar('#');
-		}
 		_putchar('\n');
-		}
 	}
 }
