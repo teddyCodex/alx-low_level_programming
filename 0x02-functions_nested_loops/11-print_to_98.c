91fa2eb8ad7cc804e4feabc7d9e235127f161fda
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-19 19:03:59 -0700

    task 11 - 4th attempt

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,26 +14,26 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d", n);
+			printf("%d\n", n);
 			if (n == 98)
 			{
 				break;
 			}
-				_putchar(',');
-				_putchar(' ');
+				putchar(',');
+				putchar(' ');
 				n++;
 		}
 	}
 	{
 		while (n >= 98)
 		{
-			printf("%d", n);
+			printf("%d\n", n);
 			if (n == 98)
 			{
 				break;
 			}
-			_putchar(',');
-			_putchar(' ');
+			putchar(',');
+			putchar(' ');
 			n--;
 		}
 	}
