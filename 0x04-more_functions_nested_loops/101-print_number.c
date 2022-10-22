d4290e52858be32fa83ae6f6d2d8397fb977987d
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-21 18:22:01 -0700

    cant be this easy

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * function that prints an integer
+ * @n: integer to be evaluated
+ * Return: Always 0 (Success)
+ */
+
+void print_number(int n)
+{
+	_putchar('n');
+}
