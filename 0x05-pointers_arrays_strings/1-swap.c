6f2b4281a774f25fae93bc58631f5fd8d2739ede
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-24 15:48:29 -0700

    swap values now!

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap values of a and b
+ * @a: value to swap with b
+ * @b: value to swap with a
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42, *b = 98;
+}
