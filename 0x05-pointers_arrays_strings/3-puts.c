d4aa5ea6e94776f4556bc56bd0b279d7dedade27
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-24 16:30:46 -0700

    puts?

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: string to be evaluated
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	_puts("%s\n", str);
+}
