09440807c4520a586bdf8bd79955462586b4a8de
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-24 16:10:59 -0700

    show me string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - function to check and return string length
+ * @s: string to be evaluated
+ * Return: Always void.
+ */
+
+int _strlen(char *s)
+{
+	_strlen(*s);
+}
