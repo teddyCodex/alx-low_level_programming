8184a42eb6bb8ea3e787b0876f48c42ab2b23c67
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-24 17:03:23 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - function that reverses a string
+ * @s: string to be evaluated
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	for (i--; i >= 0; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
