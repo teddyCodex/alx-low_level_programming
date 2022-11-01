56501d36be61c7bfd6305b0f4866b6d8c1cb2bd0
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-31 20:46:27 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,20 +12,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int a = 0, b, c;
+	int z = 0, x, y;
 
-	for (b = 0; s[b] != '\0'; b++)
+	for (x = 0; s[x] != '\0'; x++)
 	{
-		if (s[b] != 32)
+		if (s[x] != 32)
 		{
-			for (c = 0; accept[c] != '\0'; c++)
+			for (y = 0; accept[y] != '\0'; y++)
 			{
-				if (s[b] == accept[y])
-					a++;
+				if (s[x] == accept[y])
+					z++;
 			}
 		}
 		else
-			return (a);
+			return (z);
 	}
-	return (a);
+		return (z);
+
 }
