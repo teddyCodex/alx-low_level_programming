928e15daa5232e8801d0a4fbf99cf049cf3b9d93
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-31 20:44:14 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ *
+ * @s: the initial segment of
+ *
+ * @accept: which consist only of bytes from
+ *
+ * Return: the number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int a = 0, b, c;
+
+	for (b = 0; s[b] != '\0'; b++)
+	{
+		if (s[b] != 32)
+		{
+			for (c = 0; accept[c] != '\0'; c++)
+			{
+				if (s[b] == accept[y])
+					a++;
+			}
+		}
+		else
+			return (a);
+	}
+	return (a);
+}
