eeac48d72712f11bff63ba39d83b7e0dff4ada4f
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-11-07 21:13:15 -0800

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "main,h"
+
+/**
+ * factorial - factorial of n
+ * @n: int to evaluate
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	int i;
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		i = n * factorial(n - 1);
+	}
+	return (x);
+}
