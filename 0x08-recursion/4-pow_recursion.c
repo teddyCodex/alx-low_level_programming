d2b3bad99e578c5652abceb98a07a839e8e5be73
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-11-08 01:29:01 -0800

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power function
+ * @x: int to evaluate
+ * @y: int to evaluate
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
+
+	else
+	{
+		return (1);
+	}
+}
