ad16b8a296b2292d6f45ed0b7fad2293c24ada44
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-11-09 09:03:45 -0800

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the arguments it receives
+ * @argc: arg count
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i];
+
+				return (0);
+}
